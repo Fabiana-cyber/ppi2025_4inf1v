48ad51511bb40c47126bfaa9a0198ff955b0f987
Fabiana-cyber <r.fabiana4416@gmail.com>
2025-05-16 19:01:19 +0000

    Aula do dia 16/05/2025 - CSS: Criação de global e theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import './App.css';
-
+import "./styles/theme.css";
+import "./styles/global.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Meu primeiro React App</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        deserunt sapiente corporis. Iste in dolor, quibusdam ad aut eius dicta
+        esse voluptatem, deserunt dolore consequatur cum sunt eaque sequi quam.
+      </p>
     </div>
   );
 }
