92f9852169a138fd041ad81764d573f7100bef59
Fabiana-cyber <r.fabiana4416@gmail.com>
2025-05-22 19:19:33 +0000

    aula 22/05/25 - criação do componente MyText

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import { MyText } from "./components/MyText";
 function App() {
   return (
-    <div>
-      <h1>Meu primeiro React App</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-        deserunt sapiente corporis. Iste in dolor, quibusdam ad aut eius dicta
-        esse voluptatem, deserunt dolore consequatur cum sunt eaque sequi quam.
-      </p>
-    </div>
+    <>
+      <MyText />
+      <MyText />
+      <MyText />
+      <MyText />
+       
+    </>
   );
 }
 
