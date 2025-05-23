fb3d595a5f1bf872d26c8ba9af50bc8338c85d91
Fabiana-cyber <r.fabiana4416@gmail.com>
2025-05-23 18:54:31 +0000

    aula 23/05/2025 props, children e rederização usando map

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,32 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { MyText } from "./components/MyText";
 function App() {
+
+  const text = [
+    {
+      title: "Meu titulo 1",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deserunt sapiente corporis. Iste in dolor, quibusdam ad aut eius dicta esse voluptatem, deserunt dolore consequatur cum sunt eaque sequi quam.",
+    },
+    {
+      title: "Meu titulo 2",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deserunt sapiente corporis. Iste in dolor, quibusdam ad aut eius dicta esse voluptatem, deserunt dolore consequatur cum sunt eaque sequi quam.",
+    },
+    {
+      title: "Meu titulo 3",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deserunt sapiente corporis. Iste in dolor, quibusdam ad aut eius dicta esse voluptatem, deserunt dolore consequatur cum sunt eaque sequi quam.",
+    },
+    {
+      title: "Meu titulo 4",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deserunt sapiente corporis. Iste in dolor, quibusdam ad aut eius dicta esse voluptatem, deserunt dolore consequatur cum sunt eaque sequi quam.",
+    }
+  ]
   return (
     <>
-      <MyText />
-      <MyText />
-      <MyText />
-      <MyText />
-       
+    {text.map((text, index) => (
+      <MyText key={index} title={`${index + 1}. ${text.title}` }>
+        {text.text}
+      </MyText>
+    ))}
     </>
   );
 }
