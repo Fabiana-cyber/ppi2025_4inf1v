74cfc01c4078cda759199796074de8c44e59f869
Fabiana-cyber <r.fabiana4416@gmail.com>
2025-06-11 15:10:29 +0000

    codigo atualizado

--- src/components/MyTextList.jsx
+++ src/components/MyTextList.jsx
@@ -1,37 +1,52 @@
 import styles from "./MyTextList.module.css";
 import { MyText } from "./MyText";
 
-
 export function MyTextList() {
-const text = [
+  const texts = [
     {
-      title: "Meu titulo 1",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deserunt sapiente corporis. Iste in dolor, quibusdam ad aut eius dicta esse voluptatem, deserunt dolore consequatur cum sunt eaque sequi quam.",
+      title: "Lorem ipsum dolor",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quos excepturi ea enim odit vel et quia doloremque, nam accusantium eum blanditiis debitis natus inventore laborum magnam ipsum tempore itaque?",
     },
     {
-      title: "Meu titulo 2",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deserunt sapiente corporis. Iste in dolor, quibusdam ad aut eius dicta esse voluptatem, deserunt dolore consequatur cum sunt eaque sequi quam.",
+      title: "Quos, voluptatum debitis",
+      text: "Quos, voluptatum debitis, distinctio aut eaque ipsum impedit qui assumenda quis tempore libero voluptates dolore, vel inventore iste? Tempore magnam ut vero!",
     },
     {
-      title: "Meu titulo 3",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deserunt sapiente corporis. Iste in dolor, quibusdam ad aut eius dicta esse voluptatem, deserunt dolore consequatur cum sunt eaque sequi quam.",
+      title: "amet consectetur adipisicing",
+      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere sed veritatis officiis atque exercitationem vero sit necessitatibus deserunt, maxime ab expedita repellat impedit aut quae temporibus debitis voluptatem recusandae ratione.",
     },
     {
-      title: "Meu titulo 4",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deserunt sapiente corporis. Iste in dolor, quibusdam ad aut eius dicta esse voluptatem, deserunt dolore consequatur cum sunt eaque sequi quam.",
-    }
-  ]
+      title: "Lorem ipsum dolor",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quos excepturi ea enim odit vel et quia doloremque, nam accusantium eum blanditiis debitis natus inventore laborum magnam ipsum tempore itaque?",
+    },
+    {
+      title: "Quos, voluptatum debitis",
+      text: "Quos, voluptatum debitis, distinctio aut eaque ipsum impedit qui assumenda quis tempore libero voluptates dolore, vel inventore iste? Tempore magnam ut vero!",
+    },
+    // {
+    //   title: "amet consectetur adipisicing",
+    //   text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere sed veritatis officiis atque exercitationem vero sit necessitatibus deserunt, maxime ab expedita repellat impedit aut quae temporibus debitis voluptatem recusandae ratione.",
+    // },
+    // {
+    //   title: "Lorem ipsum dolor",
+    //   text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quos excepturi ea enim odit vel et quia doloremque, nam accusantium eum blanditiis debitis natus inventore laborum magnam ipsum tempore itaque?",
+    // },
+    // {
+    //   title: "Lorem ipsum dolor",
+    //   text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quos excepturi ea enim odit vel et quia doloremque, nam accusantium eum blanditiis debitis natus inventore laborum magnam ipsum tempore itaque?",
+    // },
+  ];
 
-   return (
-    <div >
-    <h1 className={styles.myTextList}> Meus Textos</h1>
+  return (
+    <div>
+      <h1 className={styles.title}>Meus textos</h1>
       <div className={styles.container}>
-      {text.map((text, index) => (
-        <MyText key={index} title={`${index + 1}. ${text.title}` }>
-          {text.text}
-        </MyText>
-      ))}
+        {texts.map((text, index) => (
+          <MyText key={index} title={`${index + 1}. ${text.title}`}>
+            {text.text}
+          </MyText>
+        ))}
       </div>
     </div>
-    );
+  );
 }
